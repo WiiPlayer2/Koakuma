21abbfd34ecaf70cb01f95c1393edda8ca7fa828
WiiPlayer2 <waldetomme@gmail.com>
2017-07-09 00:27:16 +0200

    Added Serializable attribute

--- Koakuma.Shared/Messages/BaseMessage.cs
+++ Koakuma.Shared/Messages/BaseMessage.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace Koakuma.Shared.Messages
 {
+    [Serializable]
     public class BaseMessage : CastableMessage<BaseMessage>
     {
         public virtual MessageType Type { get; protected set; }
