ed72b591994912353ce3d2445e3c9ed2ed9c8b95
WiiPlayer2 <waldetomme@gmail.com>
2017-07-09 00:25:54 +0200

    Added BasicMessage

---
+++ Koakuma.Shared/Messages/BasicMessage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koakuma.Shared.Messages
+{
+    [Serializable]
+    public class BasicMessage : BaseMessage
+    {
+        public enum ActionType
+        {
+            Control,
+            Invoke,
+            Data,
+        }
+
+        public override MessageType Type
+        {
+            get { return MessageType.Basic; }
+
+            protected set { }
+        }
+
+        public ActionType Action { get; set; }
+
+        public string Data { get; set; }
+    }
+}
--- Koakuma.Shared/Messages/MessageType.cs
+++ Koakuma.Shared/Messages/MessageType.cs
@@ -8,6 +8,7 @@ namespace Koakuma.Shared.Messages
 {
     public enum MessageType
     {
-
+        Basic,
+        Special,
     }
 }
