53a540c914a9966d40bcde37339bd222f98a3a93
WiiPlayer2 <waldetomme@gmail.com>
2017-07-09 17:53:53 +0200

    Added operators ==,!= and added Equals,GetHashCode

--- Koakuma.Shared/Messages/ModuleID.cs
+++ Koakuma.Shared/Messages/ModuleID.cs
@@ -13,5 +13,29 @@ namespace Koakuma.Shared.Messages
         public PublicKey PublicKey { get; set; }
 
         public string ModuleName { get; set; }
+
+        public static bool operator ==(ModuleID id1, ModuleID id2)
+        {
+            return id1.ModuleName == id2.ModuleName && id1.PublicKey == id2.PublicKey;
+        }
+
+        public static bool operator !=(ModuleID id1, ModuleID id2)
+        {
+            return id1.ModuleName != id2.ModuleName || id1.PublicKey != id2.PublicKey;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(obj is ModuleID)
+            {
+                return this == (ModuleID)obj;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return PublicKey.GetHashCode() * 0xFFFF + ModuleName.GetHashCode();
+        }
     }
 }
